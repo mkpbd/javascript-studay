00636c20aca55a723f2df9e20fa11845e26600b5
mkpbd <mostofakamal1990@gmail.com>
2023-06-13 17:00:59 +0600

    Variable and  Constant

---
+++ js/fundamental.js
@@ -0,0 +1,29 @@
+let user = 'John';
+let age = 25;
+let message = 'Hello';
+
+let message1;
+message1 = 'Hello!';
+message1 = 'World!'; // value changed
+alert(message1);
+
+// Uppercase constants 
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+
+const COLOR_ORANGE = "#FF7F00";
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+
+// lower case constent 
+
+const birthdays = '18.04.1982';
+const ages = someCode(birthday);
+
+// Uppercase const?
+
+const BIRTHDAY = '18.04.1982'; // make uppercase?
+const AGE = someCode(BIRTHDAY); // make uppercase?
