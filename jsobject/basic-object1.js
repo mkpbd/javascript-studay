616833d2051cd3f8e61272c5706f360edb7e58a6
mkpbd <mostofakamal1990@gmail.com>
2023-06-14 16:43:44 +0600

    check in property in exists or not exists

--- jsobject/basic-object1.js
+++ jsobject/basic-object1.js
@@ -130,3 +130,98 @@ let key2 = prompt("What do you want to know about the user?", "name");
 
 // access by variable
 alert(user8[key2]); // John (if enter "name")
+
+//=========== Computed properties     We can use square brackets in an object literal. That’s called computed properties. ===============
+
+let fruit = prompt("Which fruit to buy?", "apple");
+let bag = {
+  [fruit]: 5, // the name of the property is taken from the variable fruit
+};
+alert(bag.apple); // 5 if fruit="apple"
+
+/**
+    The meaning of a computed property is simple: [fruit] means that the property name should be taken from fruit .
+So, if a visitor enters "apple" , bag will become {apple: 5} .
+
+*/
+let fruit1 = prompt("Which fruit to buy?", "apple");
+let bag1 = {};
+// take property name from the fruit variable
+bag1[fruit1] = 5;
+
+/**
+ *
+ *
+ * We can use more complex expressions inside square brackets:
+ *
+ *
+ */
+
+let fruit3 = "apple";
+let bag3 = {
+  [fruit3 + "Computers"]: 5, // bag.appleComputers = 5
+};
+
+/**
+ *
+ * Square brackets are much more powerful than the dot notation. They allow any property names and variables. But they are also more cumbersome to write.
+ *
+ */
+
+//==================== Property value shorthand =====================
+
+// In real code we often use existing variables as values for property names.
+
+function makeUser(name, age) {
+  return {
+    name: name,
+    age: age,
+    // ...other properties
+  };
+}
+let userMake = makeUser("John", 30);
+alert(userMake.name); // John
+
+//===========  Instead of name:name we can just write name , like this:
+
+function makeUser1(name, age) {
+  return {
+    name, // same as name: name
+    age, // same as age: age
+    // ...
+  };
+}
+
+// We can use both normal properties and shorthands in the same object
+
+let user9 = {
+  name, // same as name:name
+  age: 30,
+};
+
+//============= Existence check ===============
+
+/**
+ *
+ *
+ * A notable objects feature is that it’s possible to access any property.
+ *  There will be no error if the property doesn’t exist! Accessing a non-existing property just returns undefined .
+ * It provides a very common way to test whether the property exists – to get it and compare vs undefined:
+ *
+ *
+ *
+ */
+
+let user10 = {};
+alert(user10.noSuchProperty === undefined); // true means "no such property"
+
+//=============== There also exists a special operator "in" to check for the existence of a property.  The syntax is:
+
+"key" in object;
+
+let user11 = { name: "John", age: 30 };
+alert("age" in user11); // true, user.age exists
+alert("blabla" in user11); // false, user.blabla doesn't exist
+
+
+//================= Please note that on the left side of in there must be a property name. That’s usually a quoted string. 
