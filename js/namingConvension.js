4b5a9a16d56c0ea5816d1e995ec13d83c6cd45d3
mkpbd <mostofakamal1990@gmail.com>
2023-06-14 15:27:33 +0600

    Arrow function and function expression  implemented now

--- js/namingConvension.js
+++ js/namingConvension.js
@@ -101,3 +101,165 @@ function isPrime(n) {
 function name(parameters, delimited, by, comma) {
   /* code */
 }
+
+//=============== Is "else" required? ================
+
+function checkAgeWithElse(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    // ...
+    return confirm("Did parents allow you?");
+  }
+}
+
+// ================== Will the function work differently if else is removed? =========
+
+function checkAgeWithoutElse(age) {
+  if (age > 18) {
+    return true;
+  }
+  // ...
+  return confirm("Did parents allow you?");
+}
+
+// Copy Function  Refference
+
+function sayHi() {
+  // (1) create
+  alert("Hello");
+}
+let func = sayHi; // (2) copy
+func(); // Hello // (3) run the copy (it works)!
+sayHi(); // Hello // this still works too (why wouldn't it
+
+//=================== Everything would work the same. Even more obvious what’s going on, right? ==============
+
+//============ Why is there a semicolon at the end?
+
+// You might wonder, why does Function Expression have a semicolon ; at the end, but Function Declaration does not:
+
+function sayHi1() {
+  // ...
+}
+//==================== A Function Expression is used inside the statement: =============
+let sayHi2 = function () {
+  // ...
+};
+
+//================================= Callback functions ====================
+
+/**
+ *
+ *
+ * Let’s look at more examples of passing functions as values and using function expressions. We’ll write a function ask(question, yes, no) with three parameters:
+ *
+ */
+
+/***
+ *
+ *
+ * The function should ask the question and, depending on the user’s answer, call yes() or no() :
+ *
+ *
+ */
+
+function ask(question, yes, no) {
+  // heare is  (yse is a call back functions )
+  //  no  is a Callback function
+  if (confirm(question)) yes();
+  else no();
+}
+
+function showOk() {
+  alert("You agreed.");
+}
+function showCancel() {
+  alert("You canceled the execution.");
+}
+// usage: functions showOk, showCancel are passed as arguments to ask
+ask("Do you agree?", showOk, showCancel);
+
+//================== The arguments of ask are called callback functions or just callbacks. =================
+
+function asks(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+asks(
+  "Do you agree?",
+  function () {
+    alert("You agreed.");
+  },
+  function () {
+    alert("You canceled the execution.");
+  }
+);
+
+//=================== A function is a value representing an “action” ===============
+
+/**
+ *
+ *
+ * Regular values like strings or numbers represent the data.
+ * A function can be perceived as an action.
+ * We can pass it between variables and run when we want.
+ *
+ *
+ */
+
+// Function Expression vs Function Declaration
+
+// Function Declaration
+function sumDeclaration(a, b) {
+  return a + b;
+}
+
+// Function Expression
+let sumExpression = function (a, b) {
+  return a + b;
+};
+
+// function declaration
+sayHi2("John"); // Hello, John
+function sayHi2(name) {
+  alert(`Hello, ${name}`);
+}
+
+// Function Expression
+
+sayHi3("John"); // error!
+let sayHi3 = function (name) {
+  // (*) no magic any more
+  alert(`Hello, ${name}`);
+};
+
+//=========================== Arrow functions ======================
+
+/**
+ *
+ * There’s one more very simple and concise syntax for creating functions, that’s often better than
+ * Function Expressions. It’s called “arrow functions”, because it looks like this:
+ *
+ *
+ */
+let funcs = (arg1, arg2, ...argN) => console.log("hellow");
+
+let funcc = function (arg1, arg2, ...argN) {
+  return 5;
+};
+
+let sum5 = (a, b) => a + b;
+/* The arrow function is a shorter form of:
+let sum5 = function(a, b) {
+return a + b;
+};
+*/
+alert(sum5(1, 2)); // 3
+
+//============ If we have only one argument, then parentheses can be omitted, making that even shorter: ===============
+
+// same as
+// let double = function(n) { return n * 2 }
+let double = (n) => n * 2;
+alert(double(3)); // 6
