7ac7ebf3fb151d68e2b572a2f5671e734953e585
mostofa kamal <mostofakamal1990@gmail.com>
2023-06-18 22:21:59 +0600

    own error handle

--- error/app.js
+++ error/app.js
@@ -32,3 +32,100 @@
      * 
      * 
      */
+
+
+    try {
+        alert('Start of try runs'); // (1) <--
+        // ...no errors here
+        alert('End of try runs'); // (2) <--
+        } catch(err) {
+        alert('Catch is ignored, because there are no errors'); // (3)
+        }
+        alert("...Then the execution continues");
+
+
+
+        try {
+            alert('Start of try runs'); // (1) <--
+            lalala; // error, variable is not defined!
+            alert('End of try (never reached)'); // (2)
+            } catch(err) {
+            alert(`Error has occurred!`); // (3) <--
+            }
+            alert("...Then the execution continues");
+
+
+            /**
+             * 
+             * try..catch only works for runtime errors For try..catch to work, the code must be runnable. In other words, it should be valid JavaScript.
+             * It won’t work if the code is syntactically wrong, for instance it has unmatched curly braces
+             * 
+             */
+
+
+            /// try..catch works synchronously 
+
+            // If an exception happens in “scheduled” code, like in setTimeout , then try..catch won’t catch it:
+
+            try {
+                setTimeout(function() {
+                noSuchVariable; // script will die here
+                }, 1000);
+                } catch (e) {
+                alert( "won't work" );
+                }
+
+
+                setTimeout(function() {
+                    try {
+                    noSuchVariable; // try..catch handles the error!
+                    } catch {
+                    alert( "error is caught here!" );
+                    }
+                    }, 1000);
+
+ //==================== Error object ===============
+
+ // When an error occurs, JavaScript generates an object containing the details about it. The object is then passed as an argument to catch :
+
+ try {
+    // ...
+    } catch(err) { // <-- the "error object", could use another word instead of err
+    // ...
+    }
+// For all built-in errors, the error object inside catch block has two main properties: 
+
+
+try {
+    lalala; // error, variable is not defined!
+    } catch(err) {
+    alert(err.name); // ReferenceError
+    alert(err.message); // lalala is not defined
+    alert(err.stack); // ReferenceError: lalala is not defined at ...
+    // Can also show an error as a whole
+    // The error is converted to string as "name: message"
+    alert(err); // ReferenceError: lalala is not defined
+    }
+
+    // If json is malformed, JSON.parse generates an error, so the script “dies”. 
+
+    let json = "{ bad json }";
+    try {
+    let user = JSON.parse(json); // <-- when an error occurs...
+    alert( user.name ); // doesn't work
+    } catch (e) {
+    // ...the execution jumps here
+    alert( "Our apologies, the data has errors, we'll try to request it one more time." );
+    alert( e.name );
+    alert( e.message );
+    }
+
+    //  ============== Throwing our own errors =============================
+
+    let json2 = '{ "age": 30 }'; // incomplete data
+try {
+let user2 = JSON.parse(json); // <-- no errors
+alert( user2.name ); // no name!
+} catch (e) {
+alert( "doesn't execute" );
+}
