a5a1ab8ed42f6459604008ece25a6410aa0114ac
mkpbd <mostofakamal1990@gmail.com>
2023-06-19 13:32:08 +0600

    Formating files

--- error/app.js
+++ error/app.js
@@ -1,14 +1,12 @@
-
-
 // Error handling Error handling, "try..catch"
 
 /**
- * 
- * No matter how great we are at programming, sometimes our scripts have errors. 
+ *
+ * No matter how great we are at programming, sometimes our scripts have errors.
  * They may occur because of our mistakes, an unexpected user input, an erroneous server response and for a thousand of other reasons.
- * Usually, a script “dies” (immediately stops) in case of an error, printing it to console. 
+ * Usually, a script “dies” (immediately stops) in case of an error, printing it to console.
  * But there’s a syntax construct try..catch that allows to “catch” errors and, instead of dying, do something more reasonable.
- * 
+ *
  */
 
 /**
@@ -16,123 +14,118 @@
  * The try..catch construct has two main blocks: try , and then catch :
  */
 
-
-    try {
-        // code...
-    } catch (err) {
-        // error handling
-    }
-
-    /**
-     * 
-     * 1. First, the code in try {...} is executed. 
-     * 2. If there were no errors, then catch(err) is ignored: the execution reaches the end of
-     * try and then jumps over catch .
-     * 3 If an error occurs, then try execution is stopped, and the control flows to the beginning of catch(err) . The err variable (can use any name for it) contains an error object with details about what’s happened.
-     * 
-     * 
-     */
-
-
-    try {
-        alert('Start of try runs'); // (1) <--
-        // ...no errors here
-        alert('End of try runs'); // (2) <--
-        } catch(err) {
-        alert('Catch is ignored, because there are no errors'); // (3)
-        }
-        alert("...Then the execution continues");
-
-
-
-        try {
-            alert('Start of try runs'); // (1) <--
-            lalala; // error, variable is not defined!
-            alert('End of try (never reached)'); // (2)
-            } catch(err) {
-            alert(`Error has occurred!`); // (3) <--
-            }
-            alert("...Then the execution continues");
-
-
-            /**
-             * 
-             * try..catch only works for runtime errors For try..catch to work, the code must be runnable. In other words, it should be valid JavaScript.
-             * It won’t work if the code is syntactically wrong, for instance it has unmatched curly braces
-             * 
-             */
-
-
-            /// try..catch works synchronously 
-
-            // If an exception happens in “scheduled” code, like in setTimeout , then try..catch won’t catch it:
-
-            try {
-                setTimeout(function() {
-                noSuchVariable; // script will die here
-                }, 1000);
-                } catch (e) {
-                alert( "won't work" );
-                }
-
-
-                setTimeout(function() {
-                    try {
-                    noSuchVariable; // try..catch handles the error!
-                    } catch {
-                    alert( "error is caught here!" );
-                    }
-                    }, 1000);
-
- //==================== Error object ===============
-
- // When an error occurs, JavaScript generates an object containing the details about it. The object is then passed as an argument to catch :
-
- try {
-    // ...
-    } catch(err) { // <-- the "error object", could use another word instead of err
-    // ...
-    }
-// For all built-in errors, the error object inside catch block has two main properties: 
-
-
 try {
-    lalala; // error, variable is not defined!
-    } catch(err) {
-    alert(err.name); // ReferenceError
-    alert(err.message); // lalala is not defined
-    alert(err.stack); // ReferenceError: lalala is not defined at ...
-    // Can also show an error as a whole
-    // The error is converted to string as "name: message"
-    alert(err); // ReferenceError: lalala is not defined
-    }
-
-    // If json is malformed, JSON.parse generates an error, so the script “dies”. 
-
-    let json = "{ bad json }";
-    try {
-    let user = JSON.parse(json); // <-- when an error occurs...
-    alert( user.name ); // doesn't work
-    } catch (e) {
-    // ...the execution jumps here
-    alert( "Our apologies, the data has errors, we'll try to request it one more time." );
-    alert( e.name );
-    alert( e.message );
-    }
-
-    //  ============== Throwing our own errors =============================
-
-    let json2 = '{ "age": 30 }'; // incomplete data
-try {
-let user2 = JSON.parse(json); // <-- no errors
-alert( user2.name ); // no name!
-} catch (e) {
-alert( "doesn't execute" );
+  // code...
+} catch (err) {
+  // error handling
 }
 
-//========= “Throw” operator =========================  
+/**
+ *
+ * 1. First, the code in try {...} is executed.
+ * 2. If there were no errors, then catch(err) is ignored: the execution reaches the end of
+ * try and then jumps over catch .
+ * 3 If an error occurs, then try execution is stopped, and the control flows to the beginning of catch(err) . The err variable (can use any name for it) contains an error object with details about what’s happened.
+ *
+ *
+ */
 
-// The throw operator generates an error. 
+try {
+  alert("Start of try runs"); // (1) <--
+  // ...no errors here
+  alert("End of try runs"); // (2) <--
+} catch (err) {
+  alert("Catch is ignored, because there are no errors"); // (3)
+}
+alert("...Then the execution continues");
+
+try {
+  alert("Start of try runs"); // (1) <--
+  lalala; // error, variable is not defined!
+  alert("End of try (never reached)"); // (2)
+} catch (err) {
+  alert(`Error has occurred!`); // (3) <--
+}
+alert("...Then the execution continues");
+
+/**
+ *
+ * try..catch only works for runtime errors For try..catch to work, the code must be runnable. In other words, it should be valid JavaScript.
+ * It won’t work if the code is syntactically wrong, for instance it has unmatched curly braces
+ *
+ */
+
+/// try..catch works synchronously
+
+// If an exception happens in “scheduled” code, like in setTimeout , then try..catch won’t catch it:
+
+try {
+  setTimeout(function () {
+    noSuchVariable; // script will die here
+  }, 1000);
+} catch (e) {
+  alert("won't work");
+}
+
+setTimeout(function () {
+  try {
+    noSuchVariable; // try..catch handles the error!
+  } catch {
+    alert("error is caught here!");
+  }
+}, 1000);
+
+//==================== Error object ===============
+
+// When an error occurs, JavaScript generates an object containing the details about it. The object is then passed as an argument to catch :
+
+try {
+  // ...
+} catch (err) {
+  // <-- the "error object", could use another word instead of err
+  // ...
+}
+// For all built-in errors, the error object inside catch block has two main properties:
+
+try {
+  lalala; // error, variable is not defined!
+} catch (err) {
+  alert(err.name); // ReferenceError
+  alert(err.message); // lalala is not defined
+  alert(err.stack); // ReferenceError: lalala is not defined at ...
+  // Can also show an error as a whole
+  // The error is converted to string as "name: message"
+  alert(err); // ReferenceError: lalala is not defined
+}
+
+// If json is malformed, JSON.parse generates an error, so the script “dies”.
+
+let json = "{ bad json }";
+try {
+  let user = JSON.parse(json); // <-- when an error occurs...
+  alert(user.name); // doesn't work
+} catch (e) {
+  // ...the execution jumps here
+  alert(
+    "Our apologies, the data has errors, we'll try to request it one more time."
+  );
+  alert(e.name);
+  alert(e.message);
+}
+
+//  ============== Throwing our own errors =============================
+
+let json2 = '{ "age": 30 }'; // incomplete data
+try {
+  let user2 = JSON.parse(json); // <-- no errors
+  alert(user2.name); // no name!
+} catch (e) {
+  alert("doesn't execute");
+}
+
+//========= “Throw” operator =========================
+
+// The throw operator generates an error.
 
 //throw <error object>
 
@@ -145,53 +138,48 @@ let error4 = new Error("Things happen o_O");
 alert(error4.name); // Error
 alert(error4.message); // Things happen o_O
 
-// Let’s see what kind of error JSON.parse generates: 
-
-
-try {
-    JSON.parse("{ bad json o_O }");
-    } catch(e) {
-    alert(e.name); // SyntaxError
-    alert(e.message); // Unexpected token o in JSON at position 0
-    }
-
-
-
-    let json3 = '{ "age": 30 }'; // incomplete data
-try {
-let user = JSON.parse(json3); // <-- no errors
-if (!user.name) {
-throw new SyntaxError("Incomplete data: no name"); // (*)
-}
-alert( user.name );
-} catch(e) {
-alert( "JSON Error: " + e.message ); // JSON Error: Incomplete data: no name
-}
-
-
-// Rethrowing 
+// Let’s see what kind of error JSON.parse generates:
 
 try {
-    user = JSON.parse(json); // <-- forgot to put "let" before user
-    // ...
-    } catch(err) {
-    alert("JSON Error: " + err); // JSON Error: ReferenceError: user is not defined
-    // (no JSON Error actually)
-    }
+  JSON.parse("{ bad json o_O }");
+} catch (e) {
+  alert(e.name); // SyntaxError
+  alert(e.message); // Unexpected token o in JSON at position 0
+}
 
-
-    let json4 = '{ "age": 30 }'; // incomplete data
+let json3 = '{ "age": 30 }'; // incomplete data
 try {
-let user = JSON.parse(json4);
-if (!user.name) {
-throw new SyntaxError("Incomplete data: no name");
+  let user = JSON.parse(json3); // <-- no errors
+  if (!user.name) {
+    throw new SyntaxError("Incomplete data: no name"); // (*)
+  }
+  alert(user.name);
+} catch (e) {
+  alert("JSON Error: " + e.message); // JSON Error: Incomplete data: no name
 }
-blabla(); // unexpected error
-alert( user.name );
-} catch(e) {
-if (e.name == "SyntaxError") {
-alert( "JSON Error: " + e.message );
-} else {
-throw e; // rethrow (*)
+
+// Rethrowing
+
+try {
+  user = JSON.parse(json); // <-- forgot to put "let" before user
+  // ...
+} catch (err) {
+  alert("JSON Error: " + err); // JSON Error: ReferenceError: user is not defined
+  // (no JSON Error actually)
 }
+
+let json4 = '{ "age": 30 }'; // incomplete data
+try {
+  let user = JSON.parse(json4);
+  if (!user.name) {
+    throw new SyntaxError("Incomplete data: no name");
+  }
+  blabla(); // unexpected error
+  alert(user.name);
+} catch (e) {
+  if (e.name == "SyntaxError") {
+    alert("JSON Error: " + e.message);
+  } else {
+    throw e; // rethrow (*)
+  }
 }
--- error/app3.js
+++ error/app3.js
@@ -1,182 +1,182 @@
 // Custom errors, extending Error
 
-// Extending Error 
+// Extending Error
 
 let json = `{ "name": "John", "age": 30 }`;
 
 // The "pseudocode" for the built-in Error class defined by JavaScript itself
 class Error {
-    constructor(message) {
+  constructor(message) {
     this.message = message;
     this.name = "Error"; // (different names for different built-in error classes)
     //this.stack = <nested calls>; // non-standard, but most environments support it
-    }
-    }
+  }
+}
 
-
-    class ValidationError extends Error {
-        constructor(message) {
-        super(message); // (1)
-        this.name = "ValidationError"; // (2)
-        }
-        }
-        function test() {
-
-            throw new ValidationError("Whoops!");
+class ValidationError extends Error {
+  constructor(message) {
+    super(message); // (1)
+    this.name = "ValidationError"; // (2)
+  }
+}
+function test() {
+  throw new ValidationError("Whoops!");
 }
 try {
-test();
-} catch(err) {
-alert(err.message); // Whoops!
-alert(err.name); // ValidationError
-alert(err.stack); // a list of nested calls with line numbers for each
+  test();
+} catch (err) {
+  alert(err.message); // Whoops!
+  alert(err.name); // ValidationError
+  alert(err.stack); // a list of nested calls with line numbers for each
 }
 
 class ValidationError1 extends Error {
-    constructor(message) {
+  constructor(message) {
     super(message);
     this.name = "ValidationError";
-    }
-    }
+  }
+}
 
-    // Usage
+// Usage
 function readUser(json) {
-    let user = JSON.parse(json);
-    if (!user.age) {
+  let user = JSON.parse(json);
+  if (!user.age) {
     throw new ValidationError("No field: age");
-    }
-    if (!user.name) {
+  }
+  if (!user.name) {
     throw new ValidationError("No field: name");
-    }
-    return user;
-    }
+  }
+  return user;
+}
 
-    // Working example with try..catch
+// Working example with try..catch
 try {
-    let user = readUser('{ "age": 25 }');
-    } catch (err) {
-    if (err instanceof ValidationError) {
+  let user = readUser('{ "age": 25 }');
+} catch (err) {
+  if (err instanceof ValidationError) {
     alert("Invalid data: " + err.message); // Invalid data: No field: name
-    } else if (err instanceof SyntaxError) { // (*)
+  } else if (err instanceof SyntaxError) {
+    // (*)
     alert("JSON Syntax Error: " + err.message);
-    } else {
+  } else {
     throw err; // unknown error, rethrow it (**)
-    }
-    }
+  }
+}
 
+// Further inheritance
 
-    // Further inheritance 
+class ValidationError2 extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ValidationError";
+  }
+}
+class PropertyRequiredError extends ValidationError2 {
+  constructor(property) {
+    super("No property: " + property);
+    this.name = "PropertyRequiredError";
+    this.property = property;
+  }
+}
 
-    class ValidationError2 extends Error {
-        constructor(message) {
-        super(message);
-        this.name = "ValidationError";
-        }
-        }
-        class PropertyRequiredError extends ValidationError2 {
-        constructor(property) {
-        super("No property: " + property);
-        this.name = "PropertyRequiredError";
-        this.property = property;
-        }
-        }
-
-        // Usage
+// Usage
 function readUser(json) {
-    let user = JSON.parse(json);
-    if (!user.age) {
+  let user = JSON.parse(json);
+  if (!user.age) {
     throw new PropertyRequiredError("age");
-    }
-    if (!user.name) {
+  }
+  if (!user.name) {
     throw new PropertyRequiredError("name");
-    }
+  }
 
-    return user;
+  return user;
 }
 // Working example with try..catch
 try {
-let user = readUser('{ "age": 25 }');
+  let user = readUser('{ "age": 25 }');
 } catch (err) {
-if (err instanceof ValidationError) {
-alert("Invalid data: " + err.message); // Invalid data: No property: name
-alert(err.name); // PropertyRequiredError
-alert(err.property); // name
-} else if (err instanceof SyntaxError) {
-alert("JSON Syntax Error: " + err.message);
-} else {
-throw err; // unknown error, rethrow it
+  if (err instanceof ValidationError) {
+    alert("Invalid data: " + err.message); // Invalid data: No property: name
+    alert(err.name); // PropertyRequiredError
+    alert(err.property); // name
+  } else if (err instanceof SyntaxError) {
+    alert("JSON Syntax Error: " + err.message);
+  } else {
+    throw err; // unknown error, rethrow it
+  }
 }
-}
-
 
 class MyError extends Error {
-    constructor(message) {
+  constructor(message) {
     super(message);
     this.name = this.constructor.name;
-    }
-    }
-    class ValidationError3 extends MyError { }
-    class PropertyRequiredError extends ValidationError3 {
-    constructor(property) {
+  }
+}
+class ValidationError3 extends MyError {}
+class PropertyRequiredError extends ValidationError3 {
+  constructor(property) {
     super("No property: " + property);
     this.property = property;
-    }
-    }
+  }
+}
 
-
-    // name is correct
-alert( new PropertyRequiredError("field").name ); // PropertyRequiredError
+// name is correct
+alert(new PropertyRequiredError("field").name); // PropertyRequiredError
 
 // Wrapping exceptions
 
 class ReadError extends Error {
-    constructor(message, cause) {
+  constructor(message, cause) {
     super(message);
     this.cause = cause;
-    this.name = 'ReadError';
-    }
-    }
-    class ValidationError extends Error { /*...*/ }
-    class PropertyRequiredError extends ValidationError { /* ... */ }
-    function validateUser(user) {
-    if (!user.age) {
+    this.name = "ReadError";
+  }
+}
+class ValidationError extends Error {
+  /*...*/
+}
+class PropertyRequiredError extends ValidationError {
+  /* ... */
+}
+function validateUser(user) {
+  if (!user.age) {
     throw new PropertyRequiredError("age");
-    }
-    if (!user.name) {
+  }
+  if (!user.name) {
     throw new PropertyRequiredError("name");
-    }
-    }
-
-    function readUser(json) {
-        let user;
-        try {
-        user = JSON.parse(json);
-        } catch (err) {
-        if (err instanceof SyntaxError) {
-        throw new ReadError("Syntax Error", err);
-        } else {
-        throw err;
-    }
-}
-try {
-validateUser(user);
-} catch (err) {
-if (err instanceof ValidationError) {
-throw new ReadError("Validation Error", err);
-} else {
-throw err;
-}
-}
+  }
 }
 
+function readUser(json) {
+  let user;
+  try {
+    user = JSON.parse(json);
+  } catch (err) {
+    if (err instanceof SyntaxError) {
+      throw new ReadError("Syntax Error", err);
+    } else {
+      throw err;
+    }
+  }
+  try {
+    validateUser(user);
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      throw new ReadError("Validation Error", err);
+    } else {
+      throw err;
+    }
+  }
+}
+
 try {
-    readUser('{bad json}');
-    } catch (e) {
-    if (e instanceof ReadError) {
+  readUser("{bad json}");
+} catch (e) {
+  if (e instanceof ReadError) {
     alert(e);
     // Original error: SyntaxError: Unexpected token b in JSON at position 1
     alert("Original error: " + e.cause);
-    } else {
+  } else {
     throw e;
-    }
-    }
+  }
+}
